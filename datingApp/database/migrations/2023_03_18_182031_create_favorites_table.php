2c68330f6ef0a1f4ad69366a7da1eb524a43d699
HseinKt <hseinkteish1998@gmail.com>
2023-03-18 20:34:58 +0200

    added DataBase

---
+++ datingApp/database/migrations/2023_03_18_182031_create_favorites_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->string('active');
+            $table->unsignedBigInteger('source_id');
+            $table->unsignedBigInteger('dest_id');
+            $table->timestamps();
+            $table->foreign('source_id')->references('id')->on('users');
+            $table->foreign('dest_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('favorites');
+    }
+};
