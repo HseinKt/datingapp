cd252327ad59c5d7245cc235a497223af34e0bad
HseinKt <hseinkteish1998@gmail.com>
2023-04-25 14:21:56 +0300

    added Routes

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,9 +1,11 @@
-
+import {Routes, Route} from "react-router-dom"
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Routes>
+        <Route path="/"/>
+      </Routes>
+    </>
   );
 }
 
