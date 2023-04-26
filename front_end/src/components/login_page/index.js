309c86cbebe438ef1512a12b5a98cbeb7f366a3a
HseinKt <hseinkteish1998@gmail.com>
2023-04-27 01:38:04 +0300

    added login style

--- front_end/src/components/login_page/index.js
+++ front_end/src/components/login_page/index.js
@@ -3,11 +3,11 @@ import { useState } from "react";
 const LoginPage = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [user, setUser] = useState("");
     
     const handleSubmit = async(e)=> {
         e.preventDefault(); 
     }
-
 
     return ( 
         <div className="lgn-container">
@@ -15,31 +15,31 @@ const LoginPage = () => {
                 <h1>Login</h1>
                 <form onSubmit={handleSubmit} className="form-text">
                     <div className="input-container">
-                        <label htmlFor="email">Email : </label>
                         <input 
                             type="text" 
                             id="email" 
+                            className="input"
                             placeholder="Email address" 
                             value={email}
                             onChange={(e) => setEmail(e.target.value)} 
                         />
                     </div> 
                     <div className="input-container">
-                        <label htmlFor="password">Password : </label>
                         <input 
-                            type="text" 
+                            type="password" 
                             id="password" 
+                            className="input"
                             placeholder="Password" 
                             value={password}
                             onChange={(e) => setPassword(e.target.value)} 
                         />                    
                     </div> 
-                    <div className="input-container">
+                    <div className="input-container forget-password">
                         <a href="#">Forget password?</a>
                     </div>
-                    <button type="submit" className="btn-container" id="btn-container" >Login</button>
-                    <div className="register-link">
-                         <a href="/register">does't have an account</a>
+                    <button type="submit" className="btn-container input-container" id="btn-container" >Login</button>
+                    <div className="register-link input-container">
+                         <a href="/register">Don't have an account?</a>
                     </div>
                 </form>
             </div>
