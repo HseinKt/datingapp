290841d83b5e0b87f120d06b090f2aa5ac0dfb3b
HseinKt <hseinkteish1998@gmail.com>
2023-04-28 16:33:18 +0300

    added search input component

--- front_end/src/components/search_page/search_input/index.js
+++ front_end/src/components/search_page/search_input/index.js
@@ -1,7 +1,18 @@
-const SearchInput = () => {
+
+const SearchInput = (props) => {
+
     return ( 
         <div>
-            
+            <form onSubmit={props.handleSubmit} className="searchBar">
+                <input 
+                    type="text"
+                    className="searchInput" 
+                    placeholder="Search here ..."
+                    value={props.value}
+                    onChange={props.handleChange}
+                />
+                <input type="submit" hidden/>
+            </form>
         </div>
      );
 }
--- front_end/src/pages/search_page/index.js
+++ front_end/src/pages/search_page/index.js
@@ -1,9 +1,24 @@
 import Header from "../../components/search_page/header";
+import SearchInput from "../../components/search_page/search_input";
+import { useState } from "react";
+
 
 const SearchPage = () => {
+    const [value, setValue] = useState('');
+
+    const handleChange = (e)=> {
+        e.preventDefault();
+        setValue(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return ( 
         <div>
             <Header />
+            <SearchInput value={value} handleChange={handleChange} handleSubmit={handleSubmit}/>
             Search page
         </div>
      );
