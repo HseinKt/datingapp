15a8ef943ffc0caaaed60b2e476f53b559f59248
HseinKt <hseinkteish1998@gmail.com>
2023-05-01 12:01:07 +0300

    added profile page component

--- front_end/src/components/profile_page/index.js
+++ front_end/src/components/profile_page/index.js
@@ -1,9 +1,38 @@
-const ProfilePage = () => {
+import card from "../images/card.jpg"
+
+const Profile = (props) => {
     return ( 
-        <div>
-            
+        <div className="profile-box">
+            <div className="profile-container">
+                <div className="profile-img">
+                    <img src={card} alt="profile" />
+                </div>
+                <div className="profile-details">
+                    <div className="name">
+                        <h1>{props.name}</h1>
+                    </div>
+                    <div className="line"></div>
+                    <div className="location">
+                        <h3>{props.address} {props.city}, {props.state}</h3>
+                    </div>
+                    <div className="age">
+                        {props.age} years old {props.gender}
+                    </div>
+                    <div className="about">
+                        <h3>About Me</h3>
+                        <p>
+                            {props.about}                        
+                        </p>
+                    </div>
+                    <div className="messagebtn">
+                        <button className="sendbtn " onClick={props.handleSubmit}>
+                            send a message
+                        </button>
+                    </div> 
+                </div>
+            </div>
         </div>
      );
 }
  
-export default ProfilePage;
+export default Profile;
--- front_end/src/pages/profile_page/index.js
+++ front_end/src/pages/profile_page/index.js
@@ -1,11 +1,38 @@
 import Header from "../../components/search_page/header";
-
+import Profile from "../../components/profile_page";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const ProfilePage = () => {
+    const [name, setName] = useState ("Hsein Kteish ");
+    const [age, setAge] = useState ("20 ");
+    const [gender, setGender] = useState ("man ");
+    const [about, setAbout] = useState ("Travel, surprises, music, dancing, sports, books, last minute plans, open mind, photography, museum, craziness, spontaneity, going out (but also staying in), sharing, simplicity, respect, flip flops (yes, the sandals), down to earth (however fantasy is also very important), people, casual, word, news, work, sense of humor about yourself, awareness. ");
+    const [address, setAddress] = useState ("Hamra ");
+    const [city, setCity] = useState ("Beirut ");
+    const [state, setState] = useState ("Lebanon ");
+
+
+    const navigate  = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate("/message")
+    }
+
     return ( 
         <div>
             <Header />
-            Profile Page
+            <Profile 
+                name={name} 
+                age={age} 
+                gender={gender} 
+                about={about} 
+                address={address}
+                city={city}
+                state={state}
+                handleSubmit={handleSubmit}
+            />
         </div>
      );
 }
