e9e57a5a47d6e599ea60ca97a2996d26777f2975
HseinKt <hseinkteish1998@gmail.com>
2023-05-02 17:59:11 +0300

    finished edit profile component and added upload image component

--- front_end/src/components/edit-profile/index.js
+++ front_end/src/components/edit-profile/index.js
@@ -1,7 +1,71 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import UploadImage from "./upload-image";
+
 const EditProfile = () => {
+    const navigate  = useNavigate();
+    const [name, setName] = useState ("Hsein Kteish ");
+    const [age, setAge] = useState ("20 ");
+    const [gender, setGender] = useState ("man ");
+    const [about, setAbout] = useState ("Travel, surprises, music, dancing, sports, books, last minute plans, open mind, photography, museum, craziness, spontaneity, going out (but also staying in), sharing, simplicity, respect, flip flops (yes, the sandals), down to earth (however fantasy is also very important), people, casual, word, news, work, sense of humor about yourself, awareness. ");
+    const [address, setAddress] = useState ("Hamra ");
+    const [city, setCity] = useState ("Beirut ");
+    const [state, setState] = useState ("Lebanon ");
+    const [previewImage, setPreviewImage] = useState(null);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate("/message")
+    }
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            setPreviewImage(e.target.result);
+        }
+
+        reader.readAsDataURL(file);
+    }
+
+    console.log(name);
+    
     return ( 
-        <div>
-            edit profile component
+        <div className="edit-container">
+            
+            <UploadImage previewImage={previewImage}  handleImageChange={handleImageChange}/>
+
+            <div className="edit-box-container">
+                <h3>Edit Your Profile</h3>
+                <form  className="edit-form-text">
+                    <div className="edit-input-container">
+                        <label htmlFor="name">Name</label>
+                        <input 
+                            type="text" 
+                            id="name" 
+                            className="edit-input"
+                            placeholder="Name" 
+                            value={name}
+                            onChange={(e) => setName(e.target.value)} 
+                        />
+                    </div> 
+                    <div className="edit-input-container">
+                        <label htmlFor="age">Age</label>
+                        <input 
+                            type="text" 
+                            id="age" 
+                            className="edit-input"
+                            placeholder="Age" 
+                            value={age}
+                            onChange={(e) => setAge(e.target.value)} 
+                        />                    
+                    </div> 
+                    
+                    <button type="submit" className="" id="btn-container" >Save</button>
+                    {/* btn-container edit-input-container */}
+                </form>
+            </div>
         </div>
      );
 }
---
+++ front_end/src/components/edit-profile/upload-image/index.js
@@ -0,0 +1,18 @@
+const UploadImage = (props) => {
+    return ( 
+        <div className="edit-image">
+            {/* <label htmlFor="image" className="edit-title"> Upload your image</label> */}
+            <label htmlFor="image" className="edit-label">
+                <div className="image-preview-container">
+                    {props.previewImage && (
+                        <img src={props.previewImage} alt="previewImage" className="image-preview" />
+                    )}
+                    {!props.previewImage && <div className="image-placeholder" />}
+                </div>
+            </label>
+            <input id="image" type="file" accept="images/*" onChange={props.handleImageChange} hidden/>
+        </div>
+     );
+}
+ 
+export default UploadImage;
